49a3c7f109c3b0915f87643046dc70542e55cab2
henrykash <henrykariuki003@gmail.com>
2022-07-27 00:19:15 +0300

    lots of changes on contract,scripts,hardhat,abi files

---
+++ scripts/deploy.ts
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+const main = async () => {
+  const factory = await ethers.getContractFactory("Manzi");
+  const deployedSwapper = await factory.deploy();
+
+  let data = {
+    address: deployedSwapper.address,
+    abi: JSON.parse(JSON.stringify(deployedSwapper.interface.format("json"))),
+  };
+
+  console.log(data);
+
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    process.exit(1);
+  });
